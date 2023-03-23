bd9c1bbb01633a4b10f597f717c3561bc6103614
AmberAusten <newsforamber@gmail.com>
2023-03-22 22:42:23 -0500

    Add files via upload

---
+++ Quiz2VasquezFinal copy.R
@@ -0,0 +1,48 @@
+# Amber Etana Vasquez
+# Quiz 2 Sep 3, 2021
+
+
+# Problem 1
+print(792/11)
+
+# Problem 2
+print(sqrt(pi))
+
+# Probelm 3 
+print(exp(2))
+
+#Problem 4
+print(256^(1/4))
+
+#Problem 5
+print(log(10))
+
+#Problem 6
+a = 3
+b = 7
+print(a*b)
+
+#Problem 7
+v=seq(-2,4,1)
+w=seq(1,7,1)
+print(v*w)
+# When you multiply vectors of the same size, it is done component by component, 
+# the first component times the first, the second component times the second, etc. 
+# The result shows in the v * w line
+
+
+# Probelm 8
+sum<-0
+for(i in 1:25){
+  n = (i*i)
+  sum = sum + n
+  print(sum)
+}
+
+
+
+
+
+
+
+
